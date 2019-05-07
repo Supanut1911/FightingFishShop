9eba914efb80a48e611c3cc4f2b379cd6980a14d
supanut1911 <ynotmela@gmail.com>
2019-05-07 15:43:28 +0700

    Initial Commit

---
+++ FightingFishShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FightingFishShop
+//
+//  Created by Supanut Laddayam on 7/5/2562 BE.
+//  Copyright © 2562 Supanut Laddayam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FightingFishShopTests/FightingFishShopTests.swift
@@ -0,0 +1,34 @@
+//
+//  FightingFishShopTests.swift
+//  FightingFishShopTests
+//
+//  Created by Supanut Laddayam on 7/5/2562 BE.
+//  Copyright © 2562 Supanut Laddayam. All rights reserved.
+//
+
+import XCTest
+@testable import FightingFishShop
+
+class FightingFishShopTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
